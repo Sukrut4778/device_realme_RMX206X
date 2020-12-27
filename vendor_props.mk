df91166bf7ff9f13636cdad29d751ee05962335c
rounak1619 <71965815+rounak1619@users.noreply.github.com>
2020-12-27 09:37:40 +0530

    fixed flickering

--- vendor_props.mk
+++ vendor_props.mk
@@ -55,7 +55,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display post-processing
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     ro.vendor.display.ad=1 \
     ro.vendor.display.sensortype=2 \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/OPPO_OLED_AD_calib.cfg \
